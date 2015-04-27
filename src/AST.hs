9b5f64af0e2e0211c319efa6e538c0f618a0b7d4
suppi <soupiral@gmail.com>
2015-04-27 18:28:50 +0300

    adding the &args to Fun type

--- src/AST.hs
+++ src/AST.hs
@@ -1,4 +1,5 @@
--- |The AST definition for the language
+-- |
+-- The AST definition for the language
 
 {-# LANGUAGE DeriveGeneric #-}
 
@@ -14,43 +15,55 @@ import GHC.Generics                (Generic)
 -- definitions
 ------------------
 
--- |A module definition
-data Module = Module { getModFile    :: IO.FilePath
-                     , getModName    :: Name
-                     , getModImports :: [Module]
-                     , getModExports :: Env
-                     , getModEnv     :: Env }
+-- |
+-- A module definition
+data Module = Module { getModFile           :: IO.FilePath
+                     , getModName           :: Name
+                     , getModImports        :: [Module]
+                     , getModExports        :: Env
+                     , getModExportedMacros :: Env
+                     , getModMacros         :: Env
+                     , getModEnv            :: Env }
 
--- |A require for a module.
+-- |
+-- A require for a module.
 data Require = Require FilePath Name (Maybe Name) [Name] deriving (Show, Eq, Ord)
 
--- |An environment for the interpreter.
+-- |
+-- An environment for the interpreter.
 --  used to find binded names and expression which were bound with let, letrec, let!, define or defmacro
 type Env = M.Map Name (WithMD Expr)
 
--- |holds metadata on the type
+-- |
+-- holds metadata on the type
 data WithMD a = WithMD Metadata a deriving (Eq, Generic)
 
--- |metadata, current holds the position in the interpreted file
+-- |
+-- metadata, current holds the position in the interpreted file
 type Metadata = P.SourcePos
 
--- |an alias for String
+-- |
+-- an alias for String
 type Name =  String
 
--- |a function with the environment it had when interpreted - for lexical scoping
+-- |
+-- a function with the environment it had when interpreted - for lexical scoping
 data Closure = Closure Env (WithMD Fun) deriving (Eq, Ord)
 
--- |a function is a list of argument names and a body
-data Fun = Fun [Name] Expr deriving (Eq, Ord)
+-- |
+-- a function is a list of argument names, maybe an additional 'rest' argument and a body
+data Fun = Fun [Name] (Maybe Name) Expr deriving (Eq, Ord)
 
--- |the main expression for our language
-data Expr = LIST [WithMD Expr]  -- |^a list of expressions
-          | QUOTE (WithMD Expr) -- |^quoted expression
-          | ATOM Atom           -- |^a primitive expression
-          | PROCEDURE Closure   -- |^a procedure - a closure
+-- |
+-- the main expression for our language
+data Expr = LIST [WithMD Expr]  -- ^a list of expressions
+          | QUOTE (WithMD Expr) -- ^quoted expression
+          | ATOM Atom           -- ^a primitive expression
+          | PROCEDURE Closure   -- ^a procedure - a closure
           deriving (Eq, Ord, Generic)
 
--- |primitives of the language
+-- |
+-- primitives of the language
 data Atom = Integer Integer
           | Real Double
           | Symbol Name
@@ -70,10 +83,12 @@ instance NFData Expr
 instance NFData Atom
 instance NFData (WithMD a)
 
--- |map a function on the underline type, keeps the metadata
+-- |
+-- map a function on the underline type, keeps the metadata
 instance Functor WithMD where
   fmap f (WithMD md x) = WithMD md $ f x
 
--- |I'm sure I had a reason to do that........
+-- |
+-- I'm sure I had a reason to do that........
 instance Eq a => Ord (WithMD a) where
   compare _ _ = EQ
